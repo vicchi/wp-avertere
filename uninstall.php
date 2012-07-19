652564e91a57e91580ab5ec09b26a96eac56583b
vicchi <gary@vicchi.org>
2012-07-19 12:49:26 +0100

    Remove plugin specific custom fields from post meta on uninstall

--- uninstall.php
+++ uninstall.php
@@ -3,6 +3,24 @@
 if (defined('WP_UNINSTALL_PLUGIN')) {
 	// Remove the general WP Redirect options
 	delete_option ('wp_biographia_settings');
+
+	$sql = "
+		SELECT $wpdb->posts.*
+		FROM $wpdb->posts, $wpdb->postmeta
+		WHERE $wpdb->posts.ID = $wpdb->postmeta.post_id
+		AND $wpdb->postmeta.meta_key = 'wp_redirect_url'
+		AND $wpdb->posts.post_date < NOW()
+		ORDER BY $wpdb->posts.post_date DESC";
+
+	$results = $wpdb->get_results ($sql, OBJECT);
+	if ($results) {
+		global $post;
+		foreach ($results as $post) {
+			setup_postdata ($post);
+			delete_post_meta ($post->ID, 'wp_redirect_url');
+			delete_post_meta ($post->ID, 'wp_redirect_type');
+		}	// end-foreach
+	}
 }
 
 else {
